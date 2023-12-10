6095b8b3fad1c5faf1c0f0835d21140aa31814f7
Rajeck Massa <s2848457@vuw.leidenuniv.nl>
2023-12-11 00:10:52 +0100

    Added argparser for ERASMUS

--- Client/client.py
+++ Client/client.py
@@ -5,6 +5,7 @@ import hashlib
 from Cryptodome.Cipher import AES
 import base64
 import usb.core
+import argparse
 
 # Global variables, used in multiple functions
 
@@ -13,6 +14,7 @@ checks = []
 # ALl the running jobs
 jobs = []
 connected = False
+erasmus = True
 # (Not so) private key, change this for production
 key = b"918005185E36C9888E262165401C812F"
 
@@ -20,12 +22,14 @@ key = b"918005185E36C9888E262165401C812F"
 async def obtain_hardware():
     devices = []
     usbs = usb.core.find(find_all=True)
+    # Map all found USB's & store them in a list
     for found_usb in usbs:
         result = str(found_usb.idVendor) + str(found_usb.idProduct)
         devices.append(result)
-
     return devices
 
+# Create the checksum of all the found USB's and the
+# secret file
 async def create_checksum(file_name):
     hash_md5 = hashlib.md5()
     with open(file_name, 'rb') as f:
@@ -38,16 +42,43 @@ async def create_checksum(file_name):
     return hash_md5.hexdigest()
 
 async def generate_checksums():
+    if not erasmus:
+        return
     while connected:
         checks.append(await create_checksum("supersecret/permission.json"))
         await asyncio.sleep(1)
 
-
+# Encrypt the data using AES
 async def encrypt_data(data):
     cipher = AES.new(key, AES.MODE_EAX)
     ciphertext, tag = cipher.encrypt_and_digest(bytes(data, 'utf-8'))
     send_data = cipher.nonce + tag + ciphertext
     return send_data
+
+async def generate_erasmus_response():
+    num_of_checks = len(checks)
+    res = {
+        "type": "prove",
+        "num_of_checks": num_of_checks
+    }
+    for i in range(0, num_of_checks):
+        hash = checks[i]
+        encrypted_data = await encrypt_data(hash)
+        encoded = base64.b64encode(encrypted_data)
+        res[str(i)] = encoded.decode('ascii')
+    checks.clear()
+    return json.dumps(res)
+
+async def generate_original_response():
+    current_md5_checksum = await create_checksum("supersecret/permission.json")
+    encrypted_data = await encrypt_data(current_md5_checksum)
+    encoded = base64.b64encode(encrypted_data)
+    res = {
+        "type": "prove",
+        "num_of_checks": 1,
+        "0": encoded.decode('ascii')
+    }
+    return json.dumps(res)
 
 async def handler(websocket):
     global connected
@@ -55,19 +86,10 @@ async def handler(websocket):
         msg = json.loads(message)
         if (msg["type"] == "request"):
             print("* Received verify request from server")
-            num_of_checks = len(checks)
-            res = {
-                "type": "prove",
-                "num_of_checks": num_of_checks
-            }
-            await obtain_hardware()
-            for i in range(0, num_of_checks):
-                hash = checks[i]
-                encrypted_data = await encrypt_data(hash)
-                encoded = base64.b64encode(encrypted_data)
-                res[str(i)] = encoded.decode('ascii')
-            checks.clear()
-            json_object = json.dumps(res)
+            if (erasmus):
+                json_object = await generate_erasmus_response()
+            else:
+                json_object = await generate_original_response()
             await websocket.send(json_object)
         elif (msg["type"] == "abort"):
             print("* Abort message received -- cancel program!")
@@ -80,10 +102,15 @@ async def handler(websocket):
 
 async def connect_to_server(host, port):
     global connected
+    global erasmus
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--erasmus", action=argparse.BooleanOptionalAction, default=True, help="ERASMUS or 'simple' mode")
+    args = parser.parse_args()
+    erasmus = args.erasmus
     url = "ws://" + host + ":" + str(port)
-    print("* Start connection on ", url)
     async with websockets.connect(url) as ws:
         connected = True
+        print("* Start connection on ", url)
         data = {"type": "init", "data": "clientid"}
         json_data = json.dumps(data)
         global jobs
--- Server/server.py
+++ Server/server.py
@@ -7,10 +7,15 @@ import argparse
 
 
 connections = set()
+# (not so) Secret AES key
 key = b"918005185E36C9888E262165401C812F"
-md5_key = "6f498c561258ca2185bea1fbc5eb43ed"
+# MD5 key which the client should return
+# Key RPI: 269484c03998ee530c9c76b1ffccfc94
+
+md5_key = "6a4ae8f4ea95bf0018c3f6185a498c7b"
 time_between_verifies = 0
 
+# Decrypt the messages
 async def decrypt_message(encrypted_message):
     nonce = encrypted_message[:16]
     tag = encrypted_message[16:32]
@@ -23,16 +28,22 @@ async def decrypt_message(encrypted_message):
 async def handle_prove_data(websocket):
     msg = json.loads(await websocket.recv())
     if msg["type"] == "prove":
+        # Obtain the number of checks
         num = msg["num_of_checks"]
+        print(f"Num is: {num}")
+        # Loop through them, decode them, check them
         for i in range(0, num):
             encoded_encrypted_data = msg[str(i)]
             encrypted_data = base64.b64decode(encoded_encrypted_data)
             decrypted_data = await decrypt_message(encrypted_data)
+            # If the key is different -> someone tempered with the hardware
+            # or software, so let the program quit itself
             if decrypted_data != md5_key:
                 print(f"* Tampered with client {websocket.id}! Sending abort msg")
                 data = json.dumps({"type": "abort"})
                 await websocket.send(data)
                 return
+        # If not: everything ok
         print("* All good, sending succeed msg")
         data = json.dumps({"type": "succeed"})
         await websocket.send(data)
@@ -79,4 +90,4 @@ async def main(host, port):
         await asyncio.Future()
 
 if __name__ == "__main__":
-    asyncio.run(main("localhost", 5555))
+    asyncio.run(main("127.0.0.1", 5555))
